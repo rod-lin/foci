028a5767e67776c2d13bbc5706d3cbe7d13d0fd8
lancelot <lancelot.b@outlook.com>
2017-03-21 20:12:16 +0800

    210317#02: add uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var browserify = require("browserify");
+var streamify = require("gulp-streamify");
 var source = require("vinyl-source-stream");
+var uglify = require("gulp-uglify");
 var gulp = require("gulp");
+
 
 gulp.task("default", () => {
 	var comp = file =>
 		browserify("client/" + file)
 		.bundle()
 		.pipe(source(file))
+		.pipe(streamify(uglify()))
 		.pipe(gulp.dest("test/"));
 
 	comp("fcauth.js");
