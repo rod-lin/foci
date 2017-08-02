0438f6faba3d23ac956ea87656147239f38ebbf5
rodlin <im.rod@outlook.com>
2017-08-01 22:31:43 -0400

    010817#05: fix cache

--- core/user.js
+++ core/user.js
@@ -303,7 +303,7 @@ exports.getResumeCache = async (uuid) => {
 	var resume = user.getResume();
 
 	if (!resume) {
-		exports.updateResume(uuid);
+		await exports.updateResume(uuid);
 		resume = (await exports.uuid(uuid)).getResume();
 	}
 
